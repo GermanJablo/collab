315e387ab91092574c94fa34a33058a9ff3ff5dd
EgonBolton <43938777+EgonBolton@users.noreply.github.com>
2023-04-19 00:06:32 -0300

    .

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -8,8 +8,9 @@ import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
 import * as Y from "yjs";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { type EditorState } from "lexical";
-import {type Provider} from '@lexical/yjs';
+import { type Provider } from "@lexical/yjs";
 import { IndexeddbPersistence } from "y-indexeddb";
+import { HocuspocusProvider } from "@hocuspocus/provider";
 
 export default function Editor({
   initialEditorState,
@@ -19,13 +20,13 @@ export default function Editor({
   noteId: string;
 }) {
   const saveEditorState = (editorState: EditorState) => {
-    console.log("saving editor state", noteId)
+    console.log("saving editor state", noteId);
     localStorage.setItem(noteId, JSON.stringify(editorState));
   };
 
   return (
     <LexicalComposer
-        key={noteId}
+      key={noteId}
       initialConfig={{
         editorState: null,
         namespace: "test",
@@ -50,22 +51,51 @@ export default function Editor({
 
 function createWebsocketProvider(
   id: string,
-  yjsDocMap: Map<string, Y.Doc>,
+  yjsDocMap: Map<string, Y.Doc>
 ): Provider {
   const doc = new Y.Doc();
   yjsDocMap.set(id, doc);
 
   const idbProvider = new IndexeddbPersistence(id, doc);
+  idbProvider.once("synced", () => {
+    console.log("idb");
+  });
 
-  const wsProvider = new WebsocketProvider(
-    "ws://localhost:1234",
-    id,
-    doc,
-    {
-      connect: false,
-    },
-  );
-  
+  const wsProvider = new WebsocketProvider("ws://localhost:1234", id, doc, {
+    connect: false,
+  });
+
+  const hocuspocusProvider = new HocuspocusProvider({
+    url: "ws://localhost:1234",
+    name: id,
+    document: doc,
+  });
+
+  wsProvider.on("sync", () => console.log("ws"));
+  hocuspocusProvider.on("sync", () => console.log("hocuspocus"));
+
+  // return {
+  //   connect() {
+  //     wsProvider.connect();
+  //     hocuspocusProvider.connect();
+  //   },
+
+  //   disconnect() {
+  //     wsProvider.connect();
+  //   },
+
+  //   on(event, callback) {
+  //     // ...
+  //   },
+
+  //   off(event, callback) {
+  //     // ...
+  //   },
+
+  //   // idbProvider does not do anything with awareness
+  //   awareness: wsProvider.awareness,
+  // }
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   return wsProvider;
